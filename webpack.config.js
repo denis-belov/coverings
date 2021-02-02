d328b7be7880d7b349b80bfd9b3300bb62270358
Denis Belov <dsbelov63@gmail.com>
2021-02-02 10:03:09 -0500

    up

--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,12 @@ module.exports = (env, argv) => ({
 	resolve: {
 
 		extensions: [ '.js', '.css', '.scss' ],
-		// modules: [ './node_modules' ],
-		// descriptionFiles: [ 'package.json' ],
 	},
 
 	output: {
 
 		path: path.join(__dirname, 'build'),
 		// filename: 'index.js',
-		// publicPath: '/',
 	},
 
 	module: {
@@ -147,8 +144,6 @@ module.exports = (env, argv) => ({
 
 		compress: true,
 		historyApiFallback: true,
-		// hot: true,
-		// contentBase: [ './src' ],
 		host: 'localhost',
 		port: 8080,
 		open: true,
